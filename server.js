6cb8b5464a8edf1ad3419a60e1e63efd3683fc7c
Kate Spitalnic <kespitalnic@gmail.com>
2022-02-04 14:09:52 -0500

    port added

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}!`);
   });
 
 const { programs } = require('./data/p');
